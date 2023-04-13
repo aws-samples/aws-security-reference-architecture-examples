d5a93acccaa3f07212381ca42760e1dde5b78327
Justin <kontnyj@amazon.com>
2023-04-13 09:39:30 -0400

    fixed flake8 linting issues

--- aws_sra_examples/solutions/securityhub/securityhub_org/lambda/src/securityhub.py
+++ aws_sra_examples/solutions/securityhub/securityhub_org/lambda/src/securityhub.py
@@ -343,13 +343,14 @@ def configure_member_account(account_id: str, configuration_role_name: str, regi
 
 
 def get_standard_dictionary(
-    account_id: str,
-    region: str,
-    aws_partition: str,
-    sbp_version: str,
-    cis_version: str,
-    pci_version: str,
-    nist_version: str) -> dict:
+        account_id: str,
+        region: str,
+        aws_partition: str,
+        sbp_version: str,
+        cis_version: str,
+        pci_version: str,
+        nist_version: str,
+) -> dict:
     """Get Standard ARNs.
 
     Args:
