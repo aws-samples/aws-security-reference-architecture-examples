fc9c52d9b856057db796cb51e00b626133dd9506
Liam Schneider <liamschn@amazon.com>
2024-07-29 16:40:35 -0600

    still working out mechanism to deploy rule code

--- aws_sra_examples/solutions/genai/bedrock_org/lambda/src/sra_repo.py
+++ aws_sra_examples/solutions/genai/bedrock_org/lambda/src/sra_repo.py
@@ -8,32 +8,37 @@ import os
 import shutil
 import subprocess  # noqa S404 (best practice for calling pip from script)
 import sys
-import boto3
-from botocore.exceptions import NoCredentialsError
+# import boto3
+# from botocore.exceptions import NoCredentialsError
 
 # import zipfile
 import shutil
 
-import pip
+# import pip
 
 # todo(liamschn): need to exclude "inline_" files from the staging process
 
 
-class sra_staging:
+class sra_repo:
     # Setup Default Logger
     LOGGER = logging.getLogger(__name__)
     log_level: str = os.environ.get("LOG_LEVEL", "INFO")
     LOGGER.setLevel(log_level)
 
     # class attributes # todo(liamschn): make these parameters
+    REPO_RAW_FILE_URL_PREFIX = "https://raw.githubusercontent.com/liamschn/aws-security-reference-architecture-examples/sra-genai/aws_sra_examples/solutions/genai/bedrock_org/lambda/rules/"
+    RULE_LAMBDA_FILES = {}
+    RULE_LAMBDA_FILES["sra_check_iam_users"] = "sra_check_iam_users.py"
+    REPO_BRANCH = REPO_RAW_FILE_URL_PREFIX.split("/")[5]
+
     REPO_ZIP_URL = "https://github.com/aws-samples/aws-security-reference-architecture-examples/archive/refs/heads/main.zip"
     REPO_BRANCH = REPO_ZIP_URL.split(".")[1].split("/")[len(REPO_ZIP_URL.split(".")[1].split("/")) - 1]
     STAGING_UPLOAD_FOLDER = "/tmp/sra_staging_upload"
     STAGING_TEMP_FOLDER = "/tmp/sra_temp"
     SOLUTIONS_DIR = f"/tmp/aws-security-reference-architecture-examples-{REPO_BRANCH}/aws_sra_examples/solutions"
 
-    STAGING_BUCKET: str = "sra-staging-"  # todo(liamschn): set a default value??
-    PIP_VERSION = pip.__version__
+    STAGING_BUCKET: str = "sra-staging-"  # todo(liamschn): get from SSM parameter
+    # PIP_VERSION = pip.__version__
     URLLIB3_VERSION = urllib3.__version__
 
     # class methods
@@ -78,6 +83,7 @@ class sra_staging:
                 stderr=subprocess.DEVNULL,
             )
 
+
     def zip_folder(self, path: str, zip_file: ZipFile, layer: bool = False) -> None:
         """Create a zipped file from a folder.
 
@@ -102,6 +108,16 @@ class sra_staging:
                         os.path.relpath(os.path.join(root, discovered_file), os.path.join(path, "..")),
                     )
 
+    # def download_file(self, repo_url_prefix, repo_file, local_folder):
+    #     self.LOGGER.info(f"Downloading {repo_file} file from {repo_url_prefix}")
+    #     http = urllib3.PoolManager()
+    #     # repo_code_file = http.request("GET", 
+    #     with open(f"/tmp/{local_folder}{repo_file}", 'wb') as out:
+    #         repo_code_file = http.request("GET", repo_url_prefix + repo_file)
+    #         self.LOGGER.info(f"HTTP status code: {repo_code_file.status}")
+    #         shutil.copyfileobj(repo_code_file, out)
+    #     self.LOGGER.info(f"/tmp/{local_folder} directory listing: {os.listdir('/tmp/' + local_folder)}")
+
     def download_code_library(self, repo_zip_url):
         self.LOGGER.info(f"Downloading code library from {repo_zip_url}")
         http = urllib3.PoolManager()
@@ -111,6 +127,56 @@ class sra_staging:
         zipfile.extractall("/tmp")
         self.LOGGER.info("Files extracted to /tmp")
         self.LOGGER.info(f"tmp directory listing: {os.listdir('/tmp')}")
+
+    def prepare_config_rules_for_staging(self, solution, staging_upload_folder, staging_temp_folder, solutions_dir):
+        # self.LOGGER.info(f"listing config rules for {solution}")
+        if os.path.exists(staging_upload_folder):
+            shutil.rmtree(staging_upload_folder)
+        if os.path.exists(staging_temp_folder):
+            shutil.rmtree(staging_temp_folder)
+        os.mkdir(staging_upload_folder)
+        os.mkdir(staging_temp_folder)
+
+        service_folders = os.listdir(solutions_dir)
+        for service in service_folders:
+            service_dir = solutions_dir + "/" + service
+            if os.path.isdir(service_dir):
+                service_solutions_folders = sorted(os.listdir(service_dir))
+                for solution in sorted(service_solutions_folders):
+                    if os.path.isdir(os.path.join(service_dir, solution)):
+                        self.LOGGER.info(f"Solution: {solution}")
+                        if os.path.isdir(os.path.join(service_dir, solution, "rules")): # config rules folder
+                            solution_config_rules = os.path.join(service_dir, solution, "rules")
+                            config_rule_folders = sorted(os.listdir(solution_config_rules))
+                            for config_rule in sorted(config_rule_folders):
+                                self.LOGGER.info(f"config rule: {config_rule} (in the {solution} solution)")
+                                config_rule_source_files = os.path.join(solution_config_rules, config_rule)
+                                upload_folder_name = "/sra-" + solution.replace("_", "-")
+                                config_rule_upload_folder_name = "/" + config_rule.replace("_", "-")
+                                os.mkdir(staging_temp_folder + upload_folder_name)
+                                os.mkdir(staging_temp_folder + upload_folder_name + config_rule_upload_folder_name)
+                                os.mkdir(staging_upload_folder + upload_folder_name)
+                                os.mkdir(staging_upload_folder + upload_folder_name + config_rule_upload_folder_name)
+
+                                # lambda code
+                                if os.path.exists(config_rule_source_files) and os.path.exists(os.path.join(config_rule_source_files, "requirements.txt")):
+                                    self.LOGGER.info(f"Downloading required packages for {solution} lambda...")
+                                    self.pip_install(
+                                        os.path.join(config_rule_source_files, "requirements.txt"),
+                                        staging_temp_folder + upload_folder_name + config_rule_upload_folder_name,
+                                    )
+                                for source_file in os.listdir(config_rule_source_files):
+                                    if os.path.isdir(os.path.join(config_rule_source_files, source_file)):
+                                        self.LOGGER.info(f"{source_file} is a directory, skipping...")
+                                    else:
+                                        shutil.copy(os.path.join(config_rule_source_files, source_file), staging_temp_folder + upload_folder_name + config_rule_upload_folder_name)
+                                lambda_target_folder = staging_upload_folder + upload_folder_name + config_rule_upload_folder_name + "/lambda_code"
+                                self.LOGGER.info(f"Zipping config rule code for {solution} / {config_rule} lambda to {lambda_target_folder}{config_rule_upload_folder_name}.zip...")
+                                os.mkdir(lambda_target_folder)
+                                zip_file = ZipFile(f"{lambda_target_folder}/{config_rule_upload_folder_name}.zip", "w", ZIP_DEFLATED)
+                                self.zip_folder(f"{staging_temp_folder + upload_folder_name + config_rule_upload_folder_name}/lambda", zip_file)
+                                zip_file.close()
+
 
     def prepare_code_for_staging(self, staging_upload_folder, staging_temp_folder, solutions_dir):
         if os.path.exists(staging_upload_folder):
@@ -181,26 +247,6 @@ class sra_staging:
                                 else:
                                     shutil.copy(os.path.join(cfn_template_files, cfn_template_file), cfn_templates_target_folder)
 
-        # # Debugging output:
-        # stage_dir = "staged files:\n"
-        # temp_dir = "temp files:\n"
-        # for root, dirs, files in os.walk(os.path.join(staging_upload_folder, "sra-common-prerequisites")):
-        #     for name in files:
-        #         stage_dir = stage_dir + os.path.join(root, name) + "\n"
-        #         # self.LOGGER.info(os.path.join(root, name))
-        #     for name in dirs:
-        #         stage_dir = stage_dir + os.path.join(root, name) + "\n"
-        #         # self.LOGGER.info(os.path.join(root, name))
-        # for root, dirs, files in os.walk(os.path.join(staging_temp_folder, "sra-common-prerequisites")):
-        #     for name in files:
-        #         temp_dir = temp_dir + os.path.join(root, name) + "\n"
-        #         # self.LOGGER.info(os.path.join(root, name))
-        #     for name in dirs:
-        #         temp_dir = temp_dir + os.path.join(root, name) + "\n"
-        #         # self.LOGGER.info(os.path.join(root, name))
-
-        # self.LOGGER.info(stage_dir)
-        # self.LOGGER.info(temp_dir)
 
     def stage_code_to_s3(self, directory_path, bucket_name, s3_path):
         """
