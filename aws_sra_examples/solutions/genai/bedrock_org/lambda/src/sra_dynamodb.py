bc75ee8a237bf0a1b5191c70323369561011603e
liamschn <liamschn@amazon.com>
2024-12-08 10:55:43 -0700

    fix mypy errors

--- aws_sra_examples/solutions/genai/bedrock_org/lambda/src/sra_dynamodb.py
+++ aws_sra_examples/solutions/genai/bedrock_org/lambda/src/sra_dynamodb.py
@@ -8,10 +8,11 @@ from datetime import datetime
 from time import sleep
 import botocore
 from boto3.session import Session
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Sequence
 if TYPE_CHECKING:
     from mypy_boto3_dynamodb.client import DynamoDBClient
     from mypy_boto3_dynamodb.service_resource import DynamoDBServiceResource
+    from mypy_boto3_dynamodb.type_defs import UpdateItemOutputTableTypeDef, DeleteItemOutputTableTypeDef, KeySchemaElementTypeDef, AttributeDefinitionTypeDef, ProvisionedThroughputTypeDef
 
 class sra_dynamodb:
     PROFILE = "default"
@@ -36,7 +37,7 @@ class sra_dynamodb:
         raise ValueError(f"Error creating boto3 dymanodb resource and/or client: {error}") from None
 
 
-    def __init__(self, profile="default") -> None:
+    def __init__(self, profile: str="default") -> None:
         self.PROFILE = profile
         try:
             if self.PROFILE != "default":
@@ -50,17 +51,17 @@ class sra_dynamodb:
             self.LOGGER.exception(self.UNEXPECTED)
             raise ValueError("Unexpected error!") from None
 
-    def create_table(self, table_name):
+    def create_table(self, table_name: str) -> None:
         # Define table schema
-        key_schema = [
+        key_schema: Sequence[KeySchemaElementTypeDef] = [
             {"AttributeName": "solution_name", "KeyType": "HASH"},
             {"AttributeName": "record_id", "KeyType": "RANGE"},
         ]  # Hash key  # Range key
-        attribute_definitions = [
+        attribute_definitions: Sequence[AttributeDefinitionTypeDef] = [
             {"AttributeName": "solution_name", "AttributeType": "S"},  # String type
             {"AttributeName": "record_id", "AttributeType": "S"},  # String type
         ]
-        provisioned_throughput = {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
+        provisioned_throughput: ProvisionedThroughputTypeDef = {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
 
         # Create table
         try:
@@ -81,7 +82,7 @@ class sra_dynamodb:
                 # TODO(liamschn): need to add a maximum retry mechanism here
                 sleep(5)
 
-    def table_exists(self, table_name):
+    def table_exists(self, table_name: str) -> bool:
         # Check if table exists
         try:
             self.DYNAMODB_CLIENT.describe_table(TableName=table_name)
@@ -91,15 +92,15 @@ class sra_dynamodb:
             self.LOGGER.info(f"{table_name} dynamodb table  does not exist...")
             return False
 
-    def generate_id(self):
+    def generate_id(self) -> str:
         new_record_id = str("".join(random.choice(string.ascii_letters + string.digits + "-_") for ch in range(8)))
         return new_record_id
 
-    def get_date_time(self):
+    def get_date_time(self) -> str:
         now = datetime.now()
         return now.strftime("%Y%m%d%H%M%S")
 
-    def insert_item(self, table_name, solution_name):
+    def insert_item(self, table_name: str, solution_name: str) -> tuple[str, str]:
         table = self.DYNAMODB_RESOURCE.Table(table_name)
         record_id = self.generate_id()
         date_time = self.get_date_time()
@@ -110,10 +111,9 @@ class sra_dynamodb:
                 "date_time": date_time,
             }
         )
-        # self.LOGGER.info({"insert_record_response": response})
         return record_id, date_time
 
-    def update_item(self, table_name, solution_name, record_id, attributes_and_values):
+    def update_item(self, table_name: str, solution_name: str, record_id: str, attributes_and_values: dict) -> UpdateItemOutputTableTypeDef:
         self.LOGGER.info(f"Updating {table_name} dynamodb table with {attributes_and_values}")
         table = self.DYNAMODB_RESOURCE.Table(table_name)
         update_expression = ""
@@ -136,7 +136,7 @@ class sra_dynamodb:
         )
         return response
 
-    def find_item(self, table_name, solution_name, additional_attributes) -> tuple[bool, dict]:
+    def find_item(self, table_name: str, solution_name: str, additional_attributes: dict) -> tuple[bool, dict]:
         """Find an item in the dynamodb table based on the solution name and additional attributes.
 
         Args:
@@ -164,25 +164,25 @@ class sra_dynamodb:
             "FilterExpression": filter_expression,
         }
 
-        response = table.query(**query_params)
+        response = table.query(**query_params) # type: ignore
 
         if len(response["Items"]) > 1:
             self.LOGGER.info(
-                f"Found more than one record that matched record id {response['Items'][0]['record_id']}.  Review {table_name} dynamodb table to determine cause."
+                f"Found more than one record that matched solution name {solution_name}: {additional_attributes} Review {table_name} dynamodb table to determine cause."
             )
         elif len(response["Items"]) < 1:
             return False, {}
         self.LOGGER.info(f"Found record id {response['Items'][0]}")
         return True, response["Items"][0]
 
-    def get_unique_values_from_list(self, list_of_values):
+    def get_unique_values_from_list(self, list_of_values: list) -> list:
         unique_values = []
         for value in list_of_values:
             if value not in unique_values:
                 unique_values.append(value)
         return unique_values
 
-    def get_distinct_solutions_and_accounts(self, table_name):
+    def get_distinct_solutions_and_accounts(self, table_name: str) -> tuple[list, list]:
         table = self.DYNAMODB_RESOURCE.Table(table_name)
         response = table.scan()
         solution_names = [item["solution_name"] for item in response["Items"]]
@@ -191,25 +191,21 @@ class sra_dynamodb:
         accounts = self.get_unique_values_from_list(accounts)
         return solution_names, accounts
 
-    def get_resources_for_solutions_by_account(self, table_name, solutions, account):
+    def get_resources_for_solutions_by_account(self, table_name: str, solutions: list, account: str) -> dict:
         table = self.DYNAMODB_RESOURCE.Table(table_name)
         query_results = {}
         for solution in solutions:
-            # expression_attribute_values = {":solution_name": solution}
-            # filter_expression = {":account": account}
-
             query_params = {
                 "KeyConditionExpression": "solution_name = :solution_name",
                 "ExpressionAttributeValues": {":solution_name": solution, ":account": account},
                 "FilterExpression": "account = :account",
             }
-
-            response = table.query(**query_params)
+            response = table.query(**query_params) # type: ignore
             self.LOGGER.info(f"response: {response}")
             query_results[solution] = response
         return query_results
 
-    def delete_item(self, table_name: str, solution_name: str, record_id: str) -> dict:
+    def delete_item(self, table_name: str, solution_name: str, record_id: str) -> DeleteItemOutputTableTypeDef:
         """Delete an item from the dynamodb table
 
         Args:
