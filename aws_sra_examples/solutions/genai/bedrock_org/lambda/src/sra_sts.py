19db3a704ad23476587a59754d4a37e13a120a77
liamschn <liamschn@amazon.com>
2024-12-08 16:34:28 -0700

    fix mypy errors

--- aws_sra_examples/solutions/genai/bedrock_org/lambda/src/sra_sts.py
+++ aws_sra_examples/solutions/genai/bedrock_org/lambda/src/sra_sts.py
@@ -1,9 +1,11 @@
 import logging
 import os
+from typing import Any
 
 import boto3
 import botocore
 from botocore.config import Config
+import botocore.exceptions
 
 class sra_sts:
     PROFILE = "default"
@@ -12,13 +14,15 @@ class sra_sts:
     # TODO(liamschn): this needs to be made into an SSM parameter
     CONFIGURATION_ROLE: str = ""
     BOTO3_CONFIG = Config(retries={"max_attempts": 10, "mode": "standard"})
+    PARTITION: str = ""
+    HOME_REGION: str = ""
 
     # Setup Default Logger
     LOGGER = logging.getLogger(__name__)
     log_level: str = os.environ.get("LOG_LEVEL", "INFO")
     LOGGER.setLevel(log_level)
 
-    def __init__(self, profile="default") -> None:
+    def __init__(self, profile: str="default") -> None:
         self.PROFILE = profile
         print(f"STS PROFILE INFO: {self.PROFILE}")
 
@@ -33,16 +37,14 @@ class sra_sts:
             self.STS_CLIENT = self.MANAGEMENT_ACCOUNT_SESSION.client("sts")
             self.HOME_REGION = self.MANAGEMENT_ACCOUNT_SESSION.region_name
             self.LOGGER.info(f"STS detected home region: {self.HOME_REGION}")
-            # SM_HOST_NAME = urllib.parse.urlparse(boto3.client("secretsmanager", region_name=HOME_REGION).meta.endpoint_url).hostname
-            self.PARTITION: str = self.MANAGEMENT_ACCOUNT_SESSION.get_partition_for_region(self.HOME_REGION)
-            # LOGGER.info(f"Detected management account (current account): {MANAGEMENT_ACCOUNT}")
+            self.PARTITION = self.MANAGEMENT_ACCOUNT_SESSION.get_partition_for_region(self.HOME_REGION)
         except botocore.exceptions.ClientError as error:
             if error.response["Error"]["Code"] == "ExpiredToken":
                 self.LOGGER.info("Token has expired, please re-run with proper credentials set.")
                 self.MANAGEMENT_ACCOUNT_SESSION = boto3.Session()
                 self.STS_CLIENT = self.MANAGEMENT_ACCOUNT_SESSION.client("sts")
                 self.HOME_REGION = self.MANAGEMENT_ACCOUNT_SESSION.region_name
-                self.PARTITION: str = self.MANAGEMENT_ACCOUNT_SESSION.get_partition_for_region(self.HOME_REGION)
+                self.PARTITION = self.MANAGEMENT_ACCOUNT_SESSION.get_partition_for_region(self.HOME_REGION)
 
             else:
                 self.LOGGER.info(f"Error: {error}")
@@ -59,7 +61,7 @@ class sra_sts:
                 self.LOGGER.info(f"Error: {error}")
                 raise error
 
-    def assume_role(self, account, role_name, service, region_name):
+    def assume_role(self, account: str, role_name: str, service: str, region_name: str) -> Any:
         """Get boto3 client assumed into an account for a specified service.
 
         Args:
@@ -79,19 +81,23 @@ class sra_sts:
                 RoleSessionName="SRA-AssumeCrossAccountRole",
                 DurationSeconds=900,
             )
-
-            return self.MANAGEMENT_ACCOUNT_SESSION.client(
-                service,
+            assumed_client = self.MANAGEMENT_ACCOUNT_SESSION.client(
+                service, # type: ignore
                 region_name=region_name,
                 aws_access_key_id=sts_response["Credentials"]["AccessKeyId"],
                 aws_secret_access_key=sts_response["Credentials"]["SecretAccessKey"],
                 aws_session_token=sts_response["Credentials"]["SessionToken"],
             )
+            return assumed_client
         else:
-            return self.MANAGEMENT_ACCOUNT_SESSION.client(service, region_name=region_name, config=self.BOTO3_CONFIG)
+            assumed_client = self.MANAGEMENT_ACCOUNT_SESSION.client(
+                service, # type: ignore
+                region_name=region_name, 
+                config=self.BOTO3_CONFIG)
+            return assumed_client
 
 
-    def assume_role_resource(self, account, role_name, service, region_name):
+    def assume_role_resource(self, account: str, role_name: str, service: str, region_name: str) -> Any:
         """Get boto3 resource assumed into an account for a specified service.
 
         Args:
@@ -110,20 +116,18 @@ class sra_sts:
             RoleSessionName="SRA-AssumeCrossAccountRole",
             DurationSeconds=900,
         )
-
-        return self.MANAGEMENT_ACCOUNT_SESSION.resource(
-            service,
+        assumed_resource = self.MANAGEMENT_ACCOUNT_SESSION.resource(
+            service, # type: ignore
             region_name=region_name,
             aws_access_key_id=sts_response["Credentials"]["AccessKeyId"],
             aws_secret_access_key=sts_response["Credentials"]["SecretAccessKey"],
             aws_session_token=sts_response["Credentials"]["SessionToken"],
         )
+        return assumed_resource
 
-    def get_lambda_execution_role(self):
+    def get_lambda_execution_role(self) -> str:
         try:
             response = self.STS_CLIENT.get_caller_identity()
-            # self.LOGGER.info({"get_caller_identity": response})
-            # response["UserId"], response["Account"]
             return response["Arn"]
         except Exception:
             self.LOGGER.exception(self.UNEXPECTED)
